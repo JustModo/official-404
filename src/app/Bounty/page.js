b9621a125cdfa06a37b80e84ace035908c93be0d
Yash <yashlaxman@gmail.com>
2024-07-18 02:40:34 +0530

    add masonery effect

--- src/app/Bounty/page.js
+++ src/app/Bounty/page.js
@@ -1,19 +1,61 @@
+"use client";
 import React from "react";
 
 export default function Bounty() {
   return (
-    <div className="w-full bg-white grid grid-cols-5 gap-4 grid-flow-row-dense p-5">
-      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-72 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-80 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
-      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+    <div className="columns-1 sm:columns-2 md:columns-3 lg:columns-4 mx-4">
+      <div
+        className="bg-blue-200 p-4 h-48 avbr"
+        style={{ breakInside: "avoid" }}
+      >
+        Content 1
+      </div>
+      <div className="bg-blue-200 p-4  h-64 my-4 avbr">Content 2</div>
+      <div className="bg-blue-200 p-4 h-48 my-4 avbr">
+        Content 3 with different height
+      </div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">Content 4</div>
+      <div className="bg-blue-200 p-4 h-64 my-4 avbr">
+        Content 5 with different height
+      </div>
+      <div className="bg-blue-200 p-4 h-72 my-4 avbr">Content 6</div>
+      <div className="bg-blue-200 p-4  h-64 my-4 avbr">Content 7</div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">
+        Content 8 with different height
+      </div>
+      <div className="bg-blue-200 p-4 h-48 my-4 avbr">Content 9</div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">Content 10</div>
+      <div className="bg-blue-200 p-4 h-48 my-4 avbr">
+        Content 11 with different height
+      </div>
+      <div className="bg-blue-200 p-4 h-48 my-4 avbr">Content 12</div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">Content 4</div>
+      <div className="bg-blue-200 p-4 h-64 my-4 avbr">
+        Content 5 with different height
+      </div>
+      <div className="bg-blue-200 p-4 h-72 my-4 avbr">Content 6</div>
+      <div className="bg-blue-200 p-4  h-64 my-4 avbr">Content 7</div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">
+        Content 8 with different height
+      </div>
+      <div className="bg-blue-200 p-4 h-48 my-4 avbr">Content 9</div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">Content 10</div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">Content 4</div>
+      <div className="bg-blue-200 p-4 h-64 my-4 avbr">
+        Content 5 with different height
+      </div>
+      <div className="bg-blue-200 p-4 h-72 my-4 avbr">Content 6</div>
+      <div className="bg-blue-200 p-4  h-64 my-4 avbr">Content 7</div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">
+        Content 8 with different height
+      </div>
+      <div className="bg-blue-200 p-4 h-48 my-4 avbr">Content 9</div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">Content 10</div>
+      <div className="bg-blue-200 p-4 h-48 my-4 avbr">
+        Content 11 with different height
+      </div>
+      <div className="bg-blue-200 p-4 h-48 my-4 avbr">Content 12</div>
+      <div className="bg-blue-200 p-4 h-32 my-4 avbr">Content 4</div>
     </div>
   );
 }
