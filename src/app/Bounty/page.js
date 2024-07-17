2d0886b6a3001dbd840186d8a3fc009a3e9e178a
Yash <yashlaxman@gmail.com>
2024-07-17 15:28:28 +0530

    Add bouty card template

--- src/app/Bounty/page.js
+++ src/app/Bounty/page.js
@@ -1,5 +1,19 @@
 import React from "react";
 
 export default function Bounty() {
-  return <div>Bounty</div>;
+  return (
+    <div className="w-full bg-white grid grid-cols-5 gap-4 grid-flow-row-dense p-5">
+      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-72 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-80 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+      <div className="col-span-1 h-60 bg-yellow-100">{/* First child */}</div>
+    </div>
+  );
 }
