b46239eb214887e957483a062a7ba879c6049512
vikky <139262048+VivekNeer@users.noreply.github.com>
2024-07-20 17:03:52 +0530

    Images

--- src/components/Bounty/BountyHome.jsx
+++ src/components/Bounty/BountyHome.jsx
@@ -2,32 +2,32 @@ import React from "react";
 
 export default function BountyHome() {
   return (
-    <div className="bg-neutral w-full h-128 flex flex-col justify-center items-center snap-start">
+    <div className="bg-neutral w-full h-full flex flex-col justify-center items-center snap-start">
       <div className="carousel w-full h-full">
         <div id="item1" className="carousel-item w-full h-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp"
+            src="https://picsum.photos/1920/1080"
             className="w-full h-full object-cover"
             alt="Item 1"
           />
         </div>
         <div id="item2" className="carousel-item w-full h-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp"
+            src="https://picsum.photos/1920/1080"
             className="w-full h-full object-cover"
             alt="Item 2"
           />
         </div>
         <div id="item3" className="carousel-item w-full h-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp"
+            src="https://picsum.photos/1920/1080"
             className="w-full h-full object-cover"
             alt="Item 3"
           />
         </div>
         <div id="item4" className="carousel-item w-full h-full">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp"
+            src="https://picsum.photos/1920/1080"
             className="w-full h-full object-cover"
             alt="Item 4"
           />
