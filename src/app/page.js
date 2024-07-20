0b0466fd534c6502491127d9bbce25870260697a
vikky <139262048+VivekNeer@users.noreply.github.com>
2024-07-20 16:20:45 +0530

    Added Gradient Transition to Navbar And Page

--- src/app/page.js
+++ src/app/page.js
@@ -4,14 +4,13 @@ import { motion } from "framer-motion";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center snap-start h-full">
+    <div className="flex flex-col items-center justify-center h-full w-full">
       <h1 className="text-5xl font-bold text-white">Welcome to Next.js!</h1>
-
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, loop: Infinity }}
-        className="fixed inset-0 z-[-1] bg-gradient-to-br from-[#6d94ff] to-[#5d00ff] blur-3xl opacity-50 flex justify-center align-middle"
+        className="fixed inset-0 z-[-1] blur-3xl opacity-50 flex justify-center items-center animated-gradient"
       />
     </div>
   );
